f78222fdbb75a14d874595541dc2b51939d21e79
Arthur Silverstein(bluegoo192) <silversteinarthur@gmail.com>
2016-11-23 22:46:05 -0800

    fixed pdf parsing

--- server/routes/index.js
+++ server/routes/index.js
@@ -2,12 +2,12 @@ var express = require('express');
 var router = express.Router();
 
 //PDF Parsing stuff.  will be moved later
-let fs = require('fs'),
+var fs = require('fs'),
   PDFParser = require("pdf2json");
-let pdfParser = new PDFParser(this, 1);
+var pdfParser = new PDFParser(this, 1);
 pdfParser.on("pdfParser_dataError", errData => console.error(errData.parserError));
 pdfParser.on("pdfParser_dataReady", pdfData => {
-  fs.writeFile("./pdf2json/test/pdf.content.txt", pdfParser.getRawTextContent());
+  fs.writeFile("pdf.content.txt", pdfParser.getRawTextContent());
 });
 
 /* GET home page. */
