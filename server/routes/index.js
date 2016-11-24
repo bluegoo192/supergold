c7b7246f3f014e66e760c95252c6008b7c2508d6
Arthur Silverstein(bluegoo192) <silversteinarthur@gmail.com>
2016-11-23 22:58:25 -0800

    moved parsing to a separate endpoint

--- server/routes/index.js
+++ server/routes/index.js
@@ -1,19 +1,23 @@
 var express = require('express');
 var router = express.Router();
 
-//PDF Parsing stuff.  will be moved later
-var fs = require('fs'),
-  PDFParser = require("pdf2json");
-var pdfParser = new PDFParser(this, 1);
-pdfParser.on("pdfParser_dataError", errData => console.error(errData.parserError));
-pdfParser.on("pdfParser_dataReady", pdfData => {
-  fs.writeFile("pdf.content.txt", pdfParser.getRawTextContent());
-});
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  pdfParser.loadPDF("gear-2016-17.pdf");
   res.render('index', { title: 'Express' });
+});
+
+/* Tell server to re-parse pdf.  Should only be used if we've updated the pdf */
+/* allows us to update PDF data without restarting the server */
+router.get('/refreshGEARtxt', function(req, res, next) {
+  var fs = require('fs'),
+    PDFParser = require("pdf2json");
+  var pdfParser = new PDFParser(this, 1);
+  pdfParser.on("pdfParser_dataError", errData => console.error(errData.parserError));
+  pdfParser.on("pdfParser_dataReady", pdfData => {
+    fs.writeFile("./data/gear-2016-17.txt", pdfParser.getRawTextContent());
+  });
+  pdfParser.loadPDF("./data/gear-2016-17.pdf");
 });
 
 module.exports = router;
