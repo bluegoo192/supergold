2a63b827fde11ef60383c339aef2ea059894f36c
Arthur Silverstein(bluegoo192) <silversteinarthur@gmail.com>
2016-11-23 23:08:01 -0800

    encapsulated parsing, added support for LASAR

--- server/routes/index.js
+++ server/routes/index.js
@@ -1,6 +1,17 @@
 var express = require('express');
 var router = express.Router();
 
+function parsePDF(filename) {
+  var fs = require('fs'),
+    PDFParser = require("pdf2json");
+  var pdfParser = new PDFParser(this, 1);
+  pdfParser.on("pdfParser_dataError", errData => console.error(errData.parserError));
+  pdfParser.on("pdfParser_dataReady", pdfData => {
+    fs.writeFile("./data/"+filename+".txt", pdfParser.getRawTextContent());
+  });
+  pdfParser.loadPDF("./data/"+filename+".pdf");
+}
+
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
@@ -9,16 +20,14 @@ router.get('/', function(req, res, next) {
 
 /* Tell server to re-parse pdf.  Should only be used if we've updated the pdf */
 /* allows us to update PDF data without restarting the server */
-router.get('/refreshGEARtxt', function(req, res, next) {
-  var fs = require('fs'),
-    PDFParser = require("pdf2json");
-  var pdfParser = new PDFParser(this, 1);
-  pdfParser.on("pdfParser_dataError", errData => console.error(errData.parserError));
-  pdfParser.on("pdfParser_dataReady", pdfData => {
-    fs.writeFile("./data/gear-2016-17.txt", pdfParser.getRawTextContent());
-  });
-  pdfParser.loadPDF("./data/gear-2016-17.pdf");
-  res.send("Finished updating pdf text data\n");
+router.get('/refreshGearText', function(req, res, next) {
+  parsePDF("gear-2016-17")
+  res.send("Finished updating GEAR pdf text data\n");
+});
+
+router.get('/refreshLasarText', function(req, res, next) {
+  parsePDF("lasar-2015-16")
+  res.send("Finished updating LASAR pdf text data\n");
 });
 
 module.exports = router;
